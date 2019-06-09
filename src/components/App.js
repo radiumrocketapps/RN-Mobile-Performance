6d2421f39f1ed660757ba22d1c49f215d166cfad
Lucas Malvasia <lucas.malvasia@gmail.com>
2019-06-09 14:07:35 -0300

    First structure with button, inputs and board

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Platform, StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
+import Game from './Game'
 
 // const instructions = Platform.select({
 //   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -8,14 +9,12 @@ import { Platform, StyleSheet, Text, View } from 'react-native'
 //     'Shake or press menu button for dev menu',
 // });
 
-type Props = {}
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.title}>Conways Game of Life - PWA</Text>
+        <Game />
       </View>
     )
   }
@@ -24,18 +23,12 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#efefef',
+    padding: 30
   },
-  welcome: {
+  title: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
+    fontWeight: 'bold',
+    textAlign: 'center'
   }
 })
